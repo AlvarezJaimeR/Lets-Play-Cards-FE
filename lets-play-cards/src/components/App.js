67d794d0feae28e0bb38e6a082fd9372fd1ff1cc
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-29 21:33:23 -0400

    able to grab all the userss from the BE.

---
+++ lets-play-cards/src/components/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import {Switch, Route} from "react-router-dom";
+import axios from "axios";
+
+function App() {
+  const [totalUsers, setTotalUsers] = useState([]);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    await axios.get("http://localhost:5000/api/users/")
+      .then((response) => {
+        console.log("all users", response.data);
+        setTotalUsers(response.data);
+      });
+  }
+
+  return (
+    totalUsers.length > 0 ? (
+    <div>
+      <h1>Hello World!</h1>
+        <div>
+          {totalUsers.map((user, index) => {
+            return (
+            <div 
+              key={index}>
+                <h2>
+                  {user.userName}
+                </h2>
+            </div>
+          )})}
+        </div>
+    </div>
+    ) : (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  );
+}
+
+export default App;
